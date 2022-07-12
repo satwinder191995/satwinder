a843cdd66a1571e64b443f279d6176f7066bb8e2
satwider47 <satwinder47@outlook.com>
2022-07-12 04:12:45 -0400

    browser router

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Contact from './components/Contact';
 import Project from './components/Project';
 import Certifications from './components/Certifications';
 import Footer from './components/Footer';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   // const [aboutSelected, setAboutSelected] = useState(true);
   // const [resumeSelected, setResumeSelected] = useState(false);
@@ -18,7 +18,7 @@ function App() {
       <div className="App" >
         <Nav></Nav>
       <Routes>
-  <Route path="/" element={<About/>}/>
+  <Route path="/" element={<About/>} exact/>
   <Route path="/about" element={<About/>}/>
   <Route path="/portfolio" element={<Project/>}/>
   <Route path="/contact" element={<Contact/>}/>
